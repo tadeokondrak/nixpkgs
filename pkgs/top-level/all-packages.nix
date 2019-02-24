642e0b8f8285b3ef65fd6b42217659f5c3f15b31
Tadeo Kondrak <me@tadeo.ca>
2019-02-23 23:48:19 -0700

    btpd: init at 0.16

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -16594,6 +16594,8 @@ in
 
   brave = callPackage ../applications/networking/browsers/brave { };
 
+  btpd = callPackage ../applications/networking/p2p/btpd { };
+
   notmuch-bower = callPackage ../applications/networking/mailreaders/notmuch-bower { };
 
   brig = callPackage ../applications/networking/brig { };
