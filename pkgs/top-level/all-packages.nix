93681a0ada8fe5a03d8fd24bfbbe80f89b63c303
ajs124 <git@ajs124.de>
2019-04-16 01:10:21 +0200

    pulseaudio-module-sndio: init at 12.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14865,6 +14865,8 @@ in
     pulseaudio = pulseaudioFull;
   };
 
+  pulseaudio-module-sndio = callPackage ../applications/audio/pulseaudio-module-sndio { };
+
   bluez = bluez5;
 
   bluezFull = bluez.override {
