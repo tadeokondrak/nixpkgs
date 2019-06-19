9180958d0324065721ac500bcd5c47b15463da0d
Tadeo Kondrak <me@tadeo.ca>
2019-06-18 23:31:11 -0600

    discord: add update script, run it

--- pkgs/applications/networking/instant-messengers/discord/default.nix
+++ pkgs/applications/networking/instant-messengers/discord/default.nix
@@ -3,37 +3,34 @@
 let
   inherit (pkgs) callPackage fetchurl;
 in {
-  stable = callPackage ./base.nix rec {
+  stable = callPackage ./base.nix {
     pname = "discord";
     binaryName = "Discord";
     desktopName = "Discord";
     version = "0.0.9";
     src = fetchurl {
-      url = "https://dl.discordapp.net/apps/linux/${version}/${pname}-${version}.tar.gz";
+      url = "https://dl.discordapp.net/apps/linux/0.0.9/discord-0.0.9.tar.gz";
       sha256 = "1i0f8id10rh2fx381hx151qckvvh8hbznfsfav8w0dfbd1bransf";
     };
   };
-
-  ptb = callPackage ./base.nix rec {
+  ptb = callPackage ./base.nix {
     pname = "discord-ptb";
     binaryName = "DiscordPTB";
     desktopName = "Discord PTB";
     version = "0.0.15";
     src = fetchurl {
-      url = "https://dl-ptb.discordapp.net/apps/linux/${version}/${pname}-${version}.tar.gz";
+      url = "https://dl-ptb.discordapp.net/apps/linux/0.0.15/discord-ptb-0.0.15.tar.gz";
       sha256 = "0znqb0a3yglgx7a9ypkb81jcm8kqgc6559zi7vfqn02zh15gqv6a";
     };
   };
-
-  canary = callPackage ./base.nix rec {
+  canary = callPackage ./base.nix {
     pname = "discord-canary";
     binaryName = "DiscordCanary";
     desktopName = "Discord Canary";
-    version = "0.0.77";
+    version = "0.0.83";
     src = fetchurl {
-      url = "https://dl-canary.discordapp.net/apps/linux/${version}/${pname}-${version}.tar.gz";
-      sha256 = "d32aaa3c3c2151de3ed565525934501dd818f1357b6c10217163b87e62889e2a";
+      url = "https://dl-canary.discordapp.net/apps/linux/0.0.83/discord-canary-0.0.83.tar.gz";
+      sha256 = "100a61x71cgyfm0xk4qdlbp1v41089jy7lajksbpy4y7him6bg2s";
     };
   };
 }.${branch}
-
