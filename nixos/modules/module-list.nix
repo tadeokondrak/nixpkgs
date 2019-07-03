d1ddf75f0003aef38990771b80e6ba6ee1a730cf
Tadeo Kondrak <me@tadeo.ca>
2019-07-02 19:38:38 -0600

    Revert "nixos/synapse-bt: init"
    This reverts commit c79ae8b26abcf00c35ac0e0a338a262082845f56.

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -755,7 +755,6 @@
   ./services/torrent/flexget.nix
   ./services/torrent/opentracker.nix
   ./services/torrent/peerflix.nix
-  ./services/torrent/synapse-bt.nix
   ./services/torrent/transmission.nix
   ./services/ttys/agetty.nix
   ./services/ttys/gpm.nix
